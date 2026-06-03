0ca18455da3845af36f4abc89d7329d21b7c03e1
supportmovex-prog <support.movex@gmail.com>
2026-06-03 08:40:07 +0530

    auth correct  erorr with render api

--- js/Auth.js
+++ js/Auth.js
@@ -1,124 +1,59 @@
-const API = "https://movx.onrender.com";
-
-
-
+const API = "https://movx.onrender.com/api";
 
 // ===== SIGNUP =====
-
 async function signupUser(){
-
     try{
-
         const data = {
-
             name: document.getElementById("name").value,
-
             email: document.getElementById("email").value,
-
             password: document.getElementById("password").value
-
         };
-
-
         const res = await fetch(`${API}/signup`,{
-
             method:"POST",
-
             headers:{
                 "Content-Type":"application/json"
             },
-
             body:JSON.stringify(data)
-
         });
-
-
         const result = await res.json();
-
         console.log(result);
-
         alert(result.message);
-
         window.location.href = "login.html";
-
     }
-
     catch(err){
-
         console.log(err);
-
         alert("Signup Error");
-
     }
-
 }
 
-
-
-
-
-
-
 // ===== LOGIN =====
-
 async function loginUser(){
-
     try{
-
         const data = {
-
             email: document.getElementById("email").value,
-
-            password: document.getElementById("password").value
-
+            password: document.getElementById("email").value
         };
-
-
         const res = await fetch(`${API}/login`,{
-
             method:"POST",
-
             headers:{
                 "Content-Type":"application/json"
             },
-
             body:JSON.stringify(data)
-
         });
-
-
         const result = await res.json();
-
         console.log(result);
-
-
-
         if(result.token){
-
             localStorage.setItem("token", result.token);
-
             localStorage.setItem("user", JSON.stringify(result.user));
-
             alert("✅ Login Successful");
-
             window.location.href = "index.html";
-
         }
-
         else{
-
             alert(result.message);
-
         }
-
     }
-
     catch(err){
-
         console.log(err);
-
         alert("Login Error");
-
     }
-
-} 
+}
