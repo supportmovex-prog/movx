bab79745eb0723c956e2b5e22d9c275986b6c51c
supportmovex-prog <support.movex@gmail.com>
2026-06-03 08:36:17 +0530

    server cross updated

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,21 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const app = express();
 
 require("./db");
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://supportmovex-prog.github.io", "http://localhost:5000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(express.json());
 
-
 // ROUTES
 app.use("/api", require("./routes/auth"));
-
 app.use("/api", require("./routes/booking"));
 
-
-
-app.listen(5000, ()=>{
-
-    console.log("🚀 Server Running");
-
+app.listen(process.env.PORT || 5000, () => {
+  console.log("🚀 Server Running");
 });
